ed3185ae86a6959100cc0c3762d9b2d0d72ed171
achi113s <latourgiorgio@gmail.com>
2023-04-10 15:39:31 -0400

    start word scramble app

---
+++ WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Giorgio Latour on 4/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+        }
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+        
+        // Extra validation.
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
