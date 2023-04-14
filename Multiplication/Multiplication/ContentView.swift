ef96b139680e94707e6135c915e206aeb04eb341
achi113s <latourgiorgio@gmail.com>
2023-04-14 18:47:00 -0400

    front page of multiplication app

--- Multiplication/Multiplication/ContentView.swift
+++ Multiplication/Multiplication/ContentView.swift
@@ -8,15 +8,104 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isGameActive: Bool = false
+    @State var minTimesTable: Int = 0
+    @State var maxTimesTable: Int = 10
+    @State var numberOfQuestions: Int = 0
+    
+    @State var questions: [Question] = [Question]()
+    
+    let cloudTextShadowRadius: CGFloat = 2.0
+    
+    let DeepKoamaru: Color = Color("#30336b")
+    let GreenDarnerTail: Color = Color("#74b9ff")
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color("Sky"), location: 0),
+                .init(color: Color("SkyLow"), location: 0.9)
+            ], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Text("MATH IS FUN")
+                    .font(.custom("RubikBubbles-Regular", size: 45))
+                    .foregroundColor(.white)
+                    .shadow(radius: cloudTextShadowRadius)
+                HStack {
+                    Text("HOW MANY QUESTIONS?")
+                        .font(.custom("RubikBubbles-Regular", size: 20))
+                        .foregroundColor(.white)
+                        .shadow(radius: cloudTextShadowRadius)
+                    Picker("Min Times Table", selection: $numberOfQuestions) {
+                        ForEach(2..<21) { number in
+                            Text("\(number)")
+                        }
+                    }
+                    .pickerStyle(.wheel)
+                }
+                HStack {
+                    Text("WHICH TIMES TABLES?")
+                        .font(.custom("RubikBubbles-Regular", size: 20))
+                        .foregroundColor(.white)
+                        .shadow(radius: cloudTextShadowRadius)
+                    Picker("Min Times Table", selection: $minTimesTable) {
+                        ForEach(2..<13) { number in
+                            Text("\(number)")
+                        }
+                    }
+                    .pickerStyle(.wheel)
+                    Text("TO")
+                        .font(.custom("RubikBubbles-Regular", size: 20))
+                        .foregroundColor(.white)
+                        .shadow(radius: cloudTextShadowRadius)
+                    Picker("Max Times Table", selection: $maxTimesTable) {
+                        ForEach(2..<13) { number in
+                            Text("\(number)")
+                        }
+                    }
+                    .pickerStyle(.wheel)
+                }
+                
+                Spacer()
+                
+                Button {
+                    isGameActive.toggle()
+                    
+                    generateQuestions()
+                    
+                    print(questions)
+                } label: {
+                    Text("GO!")
+                        .font(.custom("RubikBubbles-Regular", size: 70))
+                        .frame(width: 200)
+                }
+                .buttonStyle(.borderedProminent)
+                .controlSize(.large)
+                .shadow(radius: cloudTextShadowRadius)
+            }
+            .padding()
         }
-        .padding()
     }
+    
+    func generateQuestions() {
+        for _ in 0..<numberOfQuestions+2 {
+            let minNum = minTimesTable + 2
+            let maxNum = maxTimesTable + 2
+            
+            let first = Int.random(in: minNum...maxNum)
+            let second = Int.random(in: minNum...maxNum)
+            
+            let question = Question(text: "\(first) × \(second)", answer: first*second)
+            questions.append(question)
+        }
+    }
+}
+
+struct Question {
+    var text: String
+    var answer: Int
 }
 
 struct ContentView_Previews: PreviewProvider {
