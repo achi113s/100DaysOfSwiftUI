c5e82279ab885cf8aebbabdd34af208dcf20c63a
achi113s <latourgiorgio@gmail.com>
2023-04-13 22:06:59 -0400

    starting consolidation3

---
+++ Multiplication/Multiplication/MultiplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationApp.swift
+//  Multiplication
+//
+//  Created by Giorgio Latour on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
