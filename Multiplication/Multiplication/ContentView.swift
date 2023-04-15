8db1b25b7af093c8de056e59f4e564f3991e9241
achi113s <latourgiorgio@gmail.com>
2023-04-15 12:58:01 -0400

    add gameplay

--- Multiplication/Multiplication/ContentView.swift
+++ Multiplication/Multiplication/ContentView.swift
@@ -8,10 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var isGameActive: Bool = false
-    @State var minTimesTable: Int = 0
-    @State var maxTimesTable: Int = 10
-    @State var numberOfQuestions: Int = 0
+    @State private var isGameActive: Bool = false
+    @State private var minTimesTable: Int = 0
+    @State private var maxTimesTable: Int = 10
+    @State private var numberOfQuestions: Int = 0
+    
+    @State private var currentQuestion: Int = 1
+    @State private var numberCorrect: Int = 0
+    @State private var answer: String = ""
+   
+    @State private var showingResult: Bool = false
+    @State private var resultTitle: String = ""
+    @State private var resultMessage: String = ""
+    
+    @FocusState private var answerIsFocused: Bool
+    
+    var score: Double {
+        return Double(numberCorrect)/Double(numberOfQuestions+2) * 100.0
+    }
     
     @State var questions: [Question] = [Question]()
     
@@ -21,76 +35,160 @@ struct ContentView: View {
     let GreenDarnerTail: Color = Color("#74b9ff")
     
     var body: some View {
-        ZStack {
-            RadialGradient(stops: [
-                .init(color: Color("Sky"), location: 0),
-                .init(color: Color("SkyLow"), location: 0.9)
-            ], center: .top, startRadius: 200, endRadius: 700)
-            .ignoresSafeArea()
-            VStack {
-                Spacer()
-                Text("MATH IS FUN")
-                    .font(.custom("RubikBubbles-Regular", size: 45))
-                    .foregroundColor(.white)
+        if isGameActive == false {
+            ZStack {
+                RadialGradient(stops: [
+                    .init(color: Color("Sky"), location: 0),
+                    .init(color: Color("SkyLow"), location: 0.9)
+                ], center: .top, startRadius: 200, endRadius: 700)
+                .ignoresSafeArea()
+                VStack {
+                    Spacer()
+                    Text("MATH IS FUN")
+                        .font(.custom("RubikBubbles-Regular", size: 45))
+                        .foregroundColor(.white)
+                        .shadow(radius: cloudTextShadowRadius)
+                    HStack {
+                        Text("HOW MANY QUESTIONS?")
+                            .font(.custom("RubikBubbles-Regular", size: 20))
+                            .foregroundColor(.white)
+                            .shadow(radius: cloudTextShadowRadius)
+                        Picker("Min Times Table", selection: $numberOfQuestions) {
+                            ForEach(2..<21) { number in
+                                Text("\(number)")
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                    }
+                    HStack {
+                        Text("WHICH TIMES TABLES?")
+                            .font(.custom("RubikBubbles-Regular", size: 20))
+                            .foregroundColor(.white)
+                            .shadow(radius: cloudTextShadowRadius)
+                        Picker("Min Times Table", selection: $minTimesTable) {
+                            ForEach(2..<13) { number in
+                                Text("\(number)")
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                        Text("TO")
+                            .font(.custom("RubikBubbles-Regular", size: 20))
+                            .foregroundColor(.white)
+                            .shadow(radius: cloudTextShadowRadius)
+                        Picker("Max Times Table", selection: $maxTimesTable) {
+                            ForEach(2..<13) { number in
+                                Text("\(number)")
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                    }
+                    
+                    Spacer()
+                    
+                    Button {
+                        isGameActive.toggle()
+                        
+                        generateQuestions()
+                        
+                        print(questions)
+                    } label: {
+                        Text("GO!")
+                            .font(.custom("RubikBubbles-Regular", size: 70))
+                            .frame(width: 200)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .controlSize(.large)
                     .shadow(radius: cloudTextShadowRadius)
-                HStack {
-                    Text("HOW MANY QUESTIONS?")
-                        .font(.custom("RubikBubbles-Regular", size: 20))
-                        .foregroundColor(.white)
-                        .shadow(radius: cloudTextShadowRadius)
-                    Picker("Min Times Table", selection: $numberOfQuestions) {
-                        ForEach(2..<21) { number in
-                            Text("\(number)")
-                        }
-                    }
-                    .pickerStyle(.wheel)
+                    .tint(.cyan)
                 }
-                HStack {
-                    Text("WHICH TIMES TABLES?")
-                        .font(.custom("RubikBubbles-Regular", size: 20))
-                        .foregroundColor(.white)
-                        .shadow(radius: cloudTextShadowRadius)
-                    Picker("Min Times Table", selection: $minTimesTable) {
-                        ForEach(2..<13) { number in
-                            Text("\(number)")
-                        }
-                    }
-                    .pickerStyle(.wheel)
-                    Text("TO")
-                        .font(.custom("RubikBubbles-Regular", size: 20))
-                        .foregroundColor(.white)
-                        .shadow(radius: cloudTextShadowRadius)
-                    Picker("Max Times Table", selection: $maxTimesTable) {
-                        ForEach(2..<13) { number in
-                            Text("\(number)")
-                        }
-                    }
-                    .pickerStyle(.wheel)
-                }
-                
-                Spacer()
-                
-                Button {
-                    isGameActive.toggle()
-                    
-                    generateQuestions()
-                    
-                    print(questions)
-                } label: {
-                    Text("GO!")
-                        .font(.custom("RubikBubbles-Regular", size: 70))
-                        .frame(width: 200)
-                }
-                .buttonStyle(.borderedProminent)
-                .controlSize(.large)
-                .shadow(radius: cloudTextShadowRadius)
+                .padding()
             }
-            .padding()
+        } else {
+            ZStack {
+                RadialGradient(stops: [
+                    .init(color: Color("Sky"), location: 0),
+                    .init(color: Color("SkyLow"), location: 0.9)
+                ], center: .top, startRadius: 200, endRadius: 700)
+                .ignoresSafeArea()
+                VStack {
+                    HStack{
+                        Text("Question: \(currentQuestion)/\(numberOfQuestions+2)")
+                            .font(.custom("RubikBubbles-Regular", size: 25))
+                            .foregroundColor(.white)
+                            .shadow(radius: cloudTextShadowRadius)
+                        Spacer()
+                        Text("SCORE: \(score.formatted())")
+                            .font(.custom("RubikBubbles-Regular", size: 25))
+                            .foregroundColor(.white)
+                            .shadow(radius: cloudTextShadowRadius)
+                    }
+                    Spacer()
+                    VStack {
+                        Spacer()
+                        Text("WHAT IS")
+                            .font(.custom("RubikBubbles-Regular", size: 60))
+                            .foregroundColor(.white)
+                            .shadow(radius: cloudTextShadowRadius)
+                        Spacer()
+//                        Text("11 x 11")
+                        Text("\(questions[currentQuestion-1].text)")
+                            .font(.custom("RubikBubbles-Regular", size: 60))
+                            .foregroundColor(.white)
+                            .shadow(radius: cloudTextShadowRadius)
+                        Spacer()
+                        HStack {
+                            Text("ANSWER:")
+                                .font(.custom("RubikBubbles-Regular", size: 20))
+                                .foregroundColor(.white)
+                                .shadow(radius: cloudTextShadowRadius)
+                            TextField("Enter your answer", text: $answer)
+                                .keyboardType(.numberPad)
+                                .frame(height: 25)
+                                .focused($answerIsFocused)
+                        }
+                        Spacer()
+                        Button {
+                            checkAnswer(answer)
+                            showingResult.toggle()
+                            answerIsFocused = false
+                        } label: {
+                            Text("CHECK")
+                                .font(.custom("RubikBubbles-Regular", size: 25))
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .controlSize(.large)
+                        .shadow(radius: cloudTextShadowRadius)
+                        .tint(.cyan)
+                        .alert(resultTitle, isPresented: $showingResult) {
+                            Button {
+                                answer = ""
+                                if currentQuestion < numberOfQuestions + 2 {
+                                    currentQuestion += 1
+                                } else {
+                                    numberCorrect = 0
+                                    currentQuestion = 1
+                                    isGameActive.toggle()
+                                }
+                            } label: {
+                                if currentQuestion < numberOfQuestions + 2 {
+                                    Text("Next Question")
+                                } else {
+                                    Text("Reset Game")
+                                }
+                            }
+                        } message: {
+                            Text(resultMessage)
+                        }
+                    }
+                    Spacer()
+                }
+                .padding()
+            }
         }
     }
     
     func generateQuestions() {
-        for _ in 0..<numberOfQuestions+2 {
+        for _ in 0..<numberOfQuestions + 2 {
             let minNum = minTimesTable + 2
             let maxNum = maxTimesTable + 2
             
@@ -101,11 +199,48 @@ struct ContentView: View {
             questions.append(question)
         }
     }
+    
+//    if currentQuestion == numberOfQuestions {
+//        resultTitle = "DONE"
+//        resultMessage = "Your final score is: \(score)!"
+//    }
+    
+    func checkAnswer(_ answer: String) {
+        if let safeAnswer = Int(answer) {
+            if safeAnswer == questions[currentQuestion-1].answer {
+                resultTitle = "CORRECT"
+                resultMessage = "You got it right!"
+                numberCorrect += 1
+            } else {
+                resultTitle = "WRONG"
+                resultMessage = "You got it wrong!"
+            }
+            
+            if currentQuestion == numberOfQuestions + 2 {
+                resultMessage += " Your final score is: \(score)!"
+            }
+        } else {
+            resultTitle = "Can't read answer."
+            resultMessage = "Unable to parse your answer."
+        }
+    }
 }
 
 struct Question {
     var text: String
     var answer: Int
+}
+
+struct GameView: View {
+    var body: some View {
+        VStack {
+            Text("new")
+        }
+    }
+}
+
+extension Text {
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
