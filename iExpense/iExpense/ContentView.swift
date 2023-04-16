91c5488c10631091e933f73213860e1d8b66a2ca
achi113s <latourgiorgio@gmail.com>
2023-04-16 17:54:24 -0400

    new project

---
+++ iExpense/iExpense/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Giorgio Latour on 4/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("hello")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Giorgio Latour on 4/16/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
