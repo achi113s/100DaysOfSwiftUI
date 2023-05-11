06da07399fb138f111907a166572b34d08d28cec
achi113s <latourgiorgio@gmail.com>
2023-05-10 22:35:15 -0400

    bucketlist app

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Giorgio Latour on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
