520bf3564172da2cf8a959f32c7ac4fe33015b8c
achi113s <latourgiorgio@gmail.com>
2023-05-31 11:43:38 -0500

    project 16

---
+++ HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Giorgio Latour on 5/29/23.
+//
+
+import SwiftUI
+import UserNotifications
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button("Request Permission") {
+                
+            }
+            
+            Button("Schedule Notification") {
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
