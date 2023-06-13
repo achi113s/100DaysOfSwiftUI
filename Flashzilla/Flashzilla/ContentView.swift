d097a3c91dac664c4e22fc406e8b2bd05388a70f
achi113s <latourgiorgio@gmail.com>
2023-06-12 22:11:40 -0400

    accessibility environment variables

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -8,22 +8,32 @@
 import CoreHaptics
 import SwiftUI
 
+func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
+    if UIAccessibility.isReduceMotionEnabled {
+        return try body()
+    } else {
+        return try withAnimation(animation, body)
+    }
+}
+
 struct ContentView: View {
-    @Environment(\.scenePhase) var scenePhase
+    @Environment(\.accessibilityReduceMotion) var reduceMotion
+    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
+
+    @State private var scale = 1.0
     
     var body: some View {
-        VStack {
             Text("Hello, world!")
-                .onChange(of: scenePhase) { newPhase in
-                    if newPhase == .active {
-                        print("Active")
-                    } else if newPhase == .inactive {
-                        print("Inactive")
-                    } else if newPhase == .background {
-                        print("Background")
-                    }
+            .padding()
+            .scaleEffect(scale)
+            .onTapGesture {
+                withOptionalAnimation {
+                    scale *= 1.5
                 }
-        }
+            }
+            .background(reduceTransparency ? .black : .black.opacity(0.5))
+            .foregroundColor(.white)
+            .clipShape(Capsule())
     }
 }
 
