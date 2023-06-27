3a29d7adf8f0adfaec21c752bc01d58db82e8dc1
achi113s <latourgiorgio@gmail.com>
2023-06-27 13:46:03 -0400

    using groups as transparent layout guides

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,22 +7,34 @@
 
 import SwiftUI
 
-struct User: Identifiable {
-    var id = "Taylor Swift"
+struct UserView: View {
+    var body: some View {
+        Group {
+            Text("Name: Paul")
+            Text("Country: England")
+            Text("Pets: Luna and Arya")
+        }
+        .font(.title)
+    }
 }
 
 struct ContentView: View {
-    @State private var selectedUser: User? = nil
+    @State private var layoutVertically = false
     
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-                .onTapGesture {
-                    selectedUser = User()
+        Group {
+            if layoutVertically {
+                VStack {
+                    UserView()
                 }
-                .sheet(item: $selectedUser) { user in
-                    Text(user.id)
+            } else {
+                HStack {
+                    UserView()
                 }
+            }
+        }
+        .onTapGesture {
+            layoutVertically.toggle()
         }
     }
 }
