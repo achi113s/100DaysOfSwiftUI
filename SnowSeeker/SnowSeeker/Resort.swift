0faf6cc362aa8bf7e63c00433989c31f8c52c339
achi113s <latourgiorgio@gmail.com>
2023-06-27 14:36:54 -0400

    basic navigationView

---
+++ SnowSeeker/SnowSeeker/Resort.swift
@@ -0,0 +1,27 @@
+//
+//  Resort.swift
+//  SnowSeeker
+//
+//  Created by Giorgio Latour on 6/27/23.
+//
+
+import Foundation
+
+struct Resort: Codable, Identifiable {
+    let id: String
+    let name: String
+    let country: String
+    let description: String
+    let imageCredit: String
+    let price: Int
+    let size: Int
+    let snowDepth: Int
+    let elevation: Int
+    let runs: Int
+    let facilities: [String]
+    
+    // static let in a struct is essentially a lazy variable since it's only
+    // created right before it is referenced
+    static let allResorts: [Resort] = Bundle.main.decode("resorts.json")
+    static let example = allResorts[0]
+}
